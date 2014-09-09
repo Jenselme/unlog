135349e89a8f6b5efb38c4d6ba062d04dacf7446
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-09-09 20:13:24 +0200

    feat(test): test the pipe case.

--- test/test_unlog.py
+++ test/test_unlog.py
@@ -1,14 +1,23 @@
-from sh import python3
+from sh import python3, cat
 from io import StringIO
 
+
 path2main = 'unlog/main.py'
+program_output = 'test/program_output'
+program_output_filtered = 'test/program_output_filtered'
+
 
 def test_basic_filter():
-    program_output = 'test/program_output'
-    program_output_filtered = 'test/program_output_filtered'
-    
     output = StringIO()
     python3(path2main, program_output, _out=output)
     
+    with open(program_output_filtered, 'r') as correctly_filtered_output:
+        assert correctly_filtered_output.read() == output.getvalue()
+
+
+def test_basic_filter_pipe():
+    output = StringIO()
+    python3(cat(program_output), path2main, _out=output)
+
     with open(program_output_filtered, 'r') as correctly_filtered_output:
         assert correctly_filtered_output.read() == output.getvalue()
