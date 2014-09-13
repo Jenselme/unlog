cbf269a45e06f648358513299d4d89837ad7809d
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-09-13 18:38:17 +0200

    feat(config): add the possibility to include a section into another.

--- unlog/config.py
+++ unlog/config.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import configparser
+import sys
 
 try:
     from filter import Filter
@@ -9,7 +10,8 @@ except ImportError:
 
 
 class Config():
-    CONFIG_FILTER_KEYS_EXCLUDED = ['files', 'config_file', 'use_config_section']
+    CONFIG_FILTER_KEYS_EXCLUDED = ['files', 'config_file', 'use_config_section',
+                                   'include', ]
 
     def __init__(self, command_line_args):
         self._args = command_line_args  # Used to override the necessary variable
@@ -25,7 +27,7 @@ class Config():
         """Returns the Filter association with the asked section if it exits."""
         config_section = self._get_config_section(section_name)
         if config_section:
-            config_filter = self._get_config_filter(config_section)
+            config_filter = self._get_config_filter(section_name, config_section)
             return Filter(**config_filter)
 
     def _get_config_section(self, section_name):
@@ -41,7 +43,7 @@ class Config():
             if section_name in possible_section_names:
                 return config_section
 
-    def _get_config_filter(self, config_section):
+    def _get_config_filter(self, section_name, config_section):
         """Returns a dict containing the config for the Filter.
         
         Key of the in _config sections contains spaces. We need to replace them
@@ -49,6 +51,7 @@ class Config():
         named argument correctly match.
         """
         config = self._config[config_section]
+        self._include_config(section_name, config)
         config_filter = dict()
         for key, item in config.items():
             new_key = key.replace(' ', '_')
@@ -59,3 +62,27 @@ class Config():
                 config_filter[key] = item
 
         return config_filter
+
+    def _include_config(self, section_name, config):
+        """If the current config has an include directive, we include it now.
+        """
+        if 'include' in config:
+            section_to_include = config['include']
+            self._include_section(section_name, section_to_include, config)
+            # Remove the 'include' key from the dict not to pass it to the
+            # constructor of Filter.
+            del config['include']
+
+    def _include_section(self, section_name, section_to_include, config):
+        """Add the key of the section to include to config with its values unless
+        config already has a key for it.
+        """
+        if section_to_include in self._config:
+            for key, item in self._config[section_to_include].items():
+                if key not in config:
+                    config[key] = item
+        else:
+            sys.stderr.write('The include directive {} of {} doesn\'t match any'
+                             ' of the section\n'\
+                             .format(section_to_include, section_name))
+            sys.exit(1)
